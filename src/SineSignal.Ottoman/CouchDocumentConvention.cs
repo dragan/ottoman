f7ff50e8905aab8b3f8193e85e0e260e7359c999
Dale Ragan <dale.ragan@sinesignal.com>
2011-03-14 02:01:12 -0400

    Implemented a minimal document convention to be used by the document session.

--- src/SineSignal.Ottoman/CouchDocumentConvention.cs
+++ src/SineSignal.Ottoman/CouchDocumentConvention.cs
@@ -1,22 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using SineSignal.Ottoman.Generators;
 
 namespace SineSignal.Ottoman
 {
 	public class CouchDocumentConvention : ICouchDocumentConvention
 	{
-		public string IdentityPropertyName
-		{
-			get { throw new NotImplementedException(); }
-		}
+		const string DEFAULT_IDENTITY_PROPERTY_NAME = "Id";
+		
+		static readonly IDictionary<Type, object> identityGenerators = InitializeIdentityGenerators();
+		
+		public virtual string IdentityPropertyName { get { return DEFAULT_IDENTITY_PROPERTY_NAME; } }
 
-		public System.Reflection.PropertyInfo GetIdentityPropertyFor(Type entityType)
+		public PropertyInfo GetIdentityPropertyFor(Type entityType)
 		{
-			throw new NotImplementedException();
+			return entityType.GetProperty(IdentityPropertyName);
 		}
 		
 		public object GenerateIdentityFor(Type identityType)
 		{
-			throw new NotImplementedException();
+			// TODO: Introduce factory here and make sure user's can add their own generators
+			object generatedValue = null;
+			
+			if (identityType == typeof(Guid))
+			{
+				generatedValue = GetIdentityGeneratorFor<Guid>(identityType).Generate();
+			}
+			
+			return generatedValue;
+		}
+		
+		private static IDictionary<Type, object> InitializeIdentityGenerators()
+		{
+			return new Dictionary<Type, object> { { typeof(Guid), new GuidIdentityGenerator() } };
+		}
+		
+		private static IIdentityGenerator<T> GetIdentityGeneratorFor<T>(Type type)
+		{
+			object generator;
+			
+			if (identityGenerators.TryGetValue(type, out generator))
+			{
+				return generator as IIdentityGenerator<T>;
+			}
+			
+			return null;
 		}
 	}
 }
